2e5009de32136c5cdfc585ba29378c944323ce61
Faaizuddin Farooqui <ffaro3@uic.edu>
2024-04-26 18:02:19 -0500

    updated background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import "./home.css";
 
 export default function Home() {
   return (
-    <div className="hero">
-      <h1 className="hero__title">SAGE VOICE</h1>
-      <p className="hero__subtitle">Speaking with AI</p>
+    <div className="appBackground">
+      <div className="hero">
+        <h1 className="hero__title">SAGE VOICE</h1>
+        <p className="hero__subtitle">Speaking with AI</p>
 
-      <div className="hero__buttons">
-        <button className="hero__button--get-started">Get Started</button>
-        <button className="hero__button--github">Github</button>
+        <div className="hero__buttons">
+          <button className="hero__button--get-started">Get Started</button>
+          <button className="hero__button--github">Github</button>
+        </div>
       </div>
     </div>
   );
